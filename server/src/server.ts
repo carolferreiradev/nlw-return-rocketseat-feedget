92dc319aa9629092c75d75dd28aafc73f52b3c82
Carol Ferreira <developer.anacarolina@gmail.com>
2022-05-11 21:45:48 -0300

    fix: request size error fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,8 @@ import express from "express";
 import { routes } from "./routes";
 
 const app = express();
-
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
 app.use(cors());
 app.use(express.json()); //middleware
 app.use(routes);
